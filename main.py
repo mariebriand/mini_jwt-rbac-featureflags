e580480e704708d9f5611ad785576a3f00e682bb
marie <marie@MacBook-Pro-de-marie.local>
2026-01-11 14:50:41 +0100

    #6 : add basic settings

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 
+from app.config.ettings import settings
 from app.api.routers.health import router as health_router
 
 app = FastAPI(
-    title="Mini Auth Service",
-    version="0.1.0",
+    title=settings.app_name,
+    version=settings.app_version,
 )
 
 @app.get("/")
 async def hello():
-    return {"message": "Hello World!"}
+    return {"message": "Starting app: "+settings.app_name}
 
 app.include_router(health_router)
