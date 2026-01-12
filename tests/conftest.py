16b2fcbf283db20fd777e741df9f3cca8508a82c
Marie Briand <57957426+mariebriand@users.noreply.github.com>
2026-01-12 12:26:28 +0100

    #12 : add security module unit tests + fix hashed pw verification (#13)
    Co-authored-by: Marie B <marieb@Admins-MacBook-Pro.local>

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,26 +7,26 @@ from app.db.models.user import User
 
 TEST_DATABASE_URL = "sqlite:///.test.db"
 engine = create_engine(
-    TEST_DATABASE_URL,
-    echo=True,
-    connect_args={"check_same_thread": False}  # Allows multi-thread access
+	TEST_DATABASE_URL,
+	echo=True, # Will log all SQL commands executed by SQLAlchemy to the console
+	connect_args={"check_same_thread": False}  # Allows multi-thread access (normally restricted by SQLite)
 )
 
 @pytest.fixture(scope="session")
 def test_engine():
-    SQLModel.metadata.create_all(engine)
-    yield engine
-    SQLModel.metadata.drop_all(engine)
+	SQLModel.metadata.create_all(engine)
+	yield engine
+	SQLModel.metadata.drop_all(engine)
 
 @pytest.fixture(scope="function")
 def session(test_engine):
-    with Session(test_engine) as session:
-        yield session
+	with Session(test_engine) as session:
+		yield session
 
 @pytest.fixture(scope="function")
 def client(session):
-    # Override dependency
-    from app.db.session import get_session
-    app.dependency_overrides[get_session] = lambda: session
-    with TestClient(app) as c:
-        yield c
+	# Override dependency
+	from app.db.session import get_session
+	app.dependency_overrides[get_session] = lambda: session
+	with TestClient(app) as c:
+		yield c
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,9 +1,11 @@
 def test_create_user(client):
-    pass
-    user_data = {"email": "alice@example.com", "password": "secret123"}
-    response = client.post("/user/", json=user_data)
-    assert response.status_code == 201
-    data = response.json()
-    assert data["email"] == "alice@example.com"
-    assert data["is_active"] is True
-    assert "hashed_password" not in data
+	user_data = {"email": "alice@example.com", "password": "secret123"}
+	response = client.post("/user/", json=user_data)
+
+	assert response.status_code == 201
+
+	data = response.json()
+	
+	assert data["email"] == "alice@example.com"
+	assert data["is_active"] is True
+	assert "hashed_password" not in data
