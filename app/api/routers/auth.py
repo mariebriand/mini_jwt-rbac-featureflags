433d923238c71d4ff11d9a6928ec5382f3b34f65
Marie Briand <57957426+mariebriand@users.noreply.github.com>
2026-01-13 19:25:55 +0100

    #15 : add access-only routes + fix db for testing + delete db from repo and make sure it cannot be added in the future (#17)
    * #15 : add access-only routes + fix db engine for testing + add dependencies to manage roles and user extraction from token
    
    * #15 : delete db that should not be in repo
    
    ---------
    
    Co-authored-by: Marie B <marieb@Admins-MacBook-Pro.local>

--- app/api/routers/auth.py
+++ app/api/routers/auth.py
@@ -5,6 +5,7 @@ from app.db.models.user import User
 from app.db.session import get_session
 from app.core.security import verify_password
 from app.core.jwt import create_access_token
+from app.core.dependencies import require_roles
 
 router = APIRouter(tags=["auth"])
 
@@ -16,5 +17,20 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = D
 	if not user or not verify_password(form_data.password, user.hashed_password):
 		raise HTTPException(status_code=401, detail="Invalid credentials")
 
-	token = create_access_token({"sub": user.email})
+	token = create_access_token({
+		"sub": user.email,
+		"role": user.role.value
+	})
 	return {"access_token": token, "token_type": "bearer"}
+
+@router.get("/superadmins-only")
+def admin_endpoint(user: User = Depends(require_roles(["superadmin"]))):
+	return {"message": f"Hello {user.email}, you are an superadmin!"}
+
+@router.get("/admins-only")
+def user_endpoint(user: User = Depends(require_roles(["superadmin", "admin"]))):
+	return {"message": f"Hello {user.email}, you are a superadmin/admin!"}
+
+@router.get("/users-only")
+def user_endpoint(user: User = Depends(require_roles(["superadmin", "admin", "user"]))):
+	return {"message": f"Hello {user.email}, you are a superadmin/admin/user!"}
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,29 +4,37 @@ from sqlmodel import SQLModel, create_engine, Session
 from fastapi.testclient import TestClient
 from app.main import app
 from app.db.models.user import User
+from app.db.session import get_session
 
-TEST_DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(
-	TEST_DATABASE_URL,
-	echo=True, # Will log all SQL commands executed by SQLAlchemy to the console
-	connect_args={"check_same_thread": False}  # Allows multi-thread access (normally restricted by SQLite)
-)
+TEST_DATABASE_URL = "sqlite:///:memory:"
 
-@pytest.fixture(scope="session")
-def test_engine():
+@pytest.fixture
+def engine():
+	""" Create a fresh engine per test """
+	
+	engine = create_engine(
+		"sqlite:///:memory:",
+		echo=True,
+		connect_args={"check_same_thread": False}
+	)
 	SQLModel.metadata.create_all(engine)
-	yield engine
-	SQLModel.metadata.drop_all(engine)
 
-@pytest.fixture(scope="function")
-def session(test_engine):
-	with Session(test_engine) as session:
+	return engine
+
+@pytest.fixture
+def session(engine):
+	connection = engine.connect()
+	with Session(bind=connection) as session:
 		yield session
+	SQLModel.metadata.drop_all(engine)
+	connection.close()
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def client(session):
 	# Override dependency
-	from app.db.session import get_session
 	app.dependency_overrides[get_session] = lambda: session
-	with TestClient(app) as c:
-		yield c
+	
+	with TestClient(app) as client:
+		yield client
+	
+	app.dependency_overrides.clear()
