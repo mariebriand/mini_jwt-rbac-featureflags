ffd40c619947ab4251bff8c8b1cb8f070dc77e1c
Marie Briand <57957426+mariebriand@users.noreply.github.com>
2026-02-06 14:47:16 +0100

    #35 : extend authentication tests + reorganized auth (#49)
    Co-authored-by: Marie B <marieb@Admins-MacBook-Pro.local>

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ from app.db.init_db import init_db
 
 from app.api.routers.health import router as health_router
 from app.api.routers.user import router as user_router
-from app.api.routers.auth import router as auth_router
+from app.api.routers.authentication import router as authn_router
+from app.api.routers.authorization import router as authz_router
 from app.api.routers.feature_flag import router as flag_router
 
 app = FastAPI(
@@ -33,5 +34,6 @@ app.include_router(health_router)
 
 # Include routers
 app.include_router(user_router)
-app.include_router(auth_router)
+app.include_router(authn_router)
+app.include_router(authz_router)
 app.include_router(flag_router)
