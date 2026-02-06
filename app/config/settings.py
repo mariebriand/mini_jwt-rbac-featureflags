71c692c996cf2814b36ee0036a3f4828aae7fe8e
marie <marie@MacBook-Pro-de-marie.local>
2026-02-06 10:33:22 +0100

    change return of the hello endpoint

--- app/config/settings.py
+++ app/config/settings.py
@@ -4,9 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_name: str = "Mini Auth Service"
     app_version: str = "0.1.0"
-
     debug: bool = True
-
     database_url: str = "sqlite:///./auth.db"
 
     class Config:
