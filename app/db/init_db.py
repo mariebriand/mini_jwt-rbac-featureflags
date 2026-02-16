64c039ad15b222c8ff56debacba35664e0b923c3
Marie B <marieb@Admins-MacBook-Pro.local>
2026-02-16 16:19:20 +0100

    #70 : fix CI

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel
-from app.db.session import engine
-from app.db.models import User, FeatureFlag
+from app.db.session import get_engine
 
 
 def init_db():
+    engine = get_engine()
     SQLModel.metadata.create_all(engine)
