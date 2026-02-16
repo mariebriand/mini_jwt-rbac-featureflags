de160f2d3f5241b73609b7ec5217648006535479
Marie B <marieb@Admins-MacBook-Pro.local>
2026-02-16 16:11:33 +0100

    #70 : reformat

--- app/db/session.py
+++ app/db/session.py
@@ -1,11 +1,9 @@
 from sqlmodel import create_engine, Session
 from app.core.config import settings
 
+
 def get_engine():
-    return create_engine(
-        settings.database_url,
-        echo=settings.debug
-        )
+    return create_engine(settings.database_url, echo=settings.debug)
 
 
 def get_session():
