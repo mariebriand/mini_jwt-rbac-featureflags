4afc3e94042f21ab009b50b3230cfbdf4d7c460d
Marie B <marieb@Admins-MacBook-Pro.local>
2026-02-16 16:10:24 +0100

    #70 : fix CI

--- app/db/session.py
+++ app/db/session.py
@@ -1,9 +1,14 @@
 from sqlmodel import create_engine, Session
 from app.core.config import settings
 
-engine = create_engine(settings.database_url, echo=settings.debug)
+def get_engine():
+    return create_engine(
+        settings.database_url,
+        echo=settings.debug
+        )
 
 
 def get_session():
+    engine = get_engine()
     with Session(engine) as session:
         yield session
